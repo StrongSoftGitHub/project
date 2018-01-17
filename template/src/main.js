908d8efee6571a7db3a6f8da71186146a9c87e7f
罗维辉 <lwh1@strongsoft.net>
2018-01-17 14:34:33 +0800

    修改 模板调整

--- template/src/main.js
+++ template/src/main.js
@@ -2,7 +2,20 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// 清除浏览器默认的样式  
+import 'reset.css'
+// 全局状态管理器
+import store from '@/store' 
+// 路由配置
+import router from '@/router'
+// API配置信息
+import apiConfig from '@/api'
+// 接口请求工具
+import $api from '@strongsoft/vue-axios'
+
+Vue.use($api,{
+  apiConfig
+})
 
 Vue.config.productionTip = false
 
